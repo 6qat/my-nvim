839b29b34f98b897dc4c7c62f3a25fa31ab4c5e7
Guilherme C. B. Moreira <ceschiatti@gmail.com>
2024-03-10 20:52:24 -0300

    Improving keymaps

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set nu")
 
 vim.g.mapleader = " "
 
@@ -34,6 +35,15 @@ local plugins = {
   {
     "nvim-treesitter/nvim-treesitter", 
     build = ":TSUpdate"
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
   }
 }
 
@@ -44,6 +54,27 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+-- nvimtree
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree focus<CR>", { desc = "Nvimtree Focus window" })
+vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle filesystem reveal left<CR>"  , {})
+
+
+vim.keymap.set("i", "<C-b>", "<ESC>^i", { desc = "Move Beginning of line" })
+vim.keymap.set("i", "<C-e>", "<End>", { desc = "Move End of line" })
+vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Move Left" })
+vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Move Right" })
+vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Move Down" })
+vim.keymap.set("i", "<C-k>", "<Up>", { desc = "Move Up" })
+
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Switch Window left" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Switch Window right" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Switch Window down" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Switch Window up" })
+
+vim.keymap.set("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "Toggle Line number" })
+vim.keymap.set("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "Toggle Relative number" })
 
 local config = require("nvim-treesitter.configs")
 config.setup({
